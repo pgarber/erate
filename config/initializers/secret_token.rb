1f9e718839342dccc96763c47fdc4186b193c318
pgarber <pgarber108@gmail.com>
2014-01-15 15:47:04 -0500

    Refinery default

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erate::Application.config.secret_token = '2b05d6b9b36bc85d8c1dae8ce3cf5b5bb2012456ed1fe12950cd1b5ed721e78edbf28ece0695b6000569f791cd15f41c3cc195d91136b26c9815b4567e74441c'
